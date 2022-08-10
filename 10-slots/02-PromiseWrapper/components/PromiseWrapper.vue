4970589e03cd6a9a447d64ffdcf6cdc209ddc535
starasov <starasov@sportmaster.ru>
2022-08-11 00:06:19 +0300

    Решены задачи 10

--- 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
+++ 10-slots/02-PromiseWrapper/components/PromiseWrapper.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- -->
+  <slot :name="state" :[prop]="result" />
 </template>
 
 <script>
@@ -10,6 +10,40 @@ export default {
     promise: {
       type: Promise,
       required: true,
+    },
+  },
+
+  data() {
+    return {
+      state: null,
+      result: null,
+    };
+  },
+
+  computed: {
+    prop() {
+      if (this.state === 'fulfilled') return 'result';
+      else if (this.state === 'rejected') return 'error';
+      else return null;
+    },
+  },
+
+  watch: {
+    promise: {
+      handler(promise) {
+        this.state = 'pending';
+        this.result = null;
+        promise
+          .then((result) => {
+            this.state = 'fulfilled';
+            this.result = result;
+          })
+          .catch((error) => {
+            this.state = 'rejected';
+            this.result = error;
+          });
+      },
+      immediate: true,
     },
   },
 };
